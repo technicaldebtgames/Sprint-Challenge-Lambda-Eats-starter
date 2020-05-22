2da4b0673f7d88d3df1f0df7d461990a828620f6
Dan Brioli <technicaldebtgames@gmail.com>
2020-05-22 16:57:42 -0400

    some stretch testing

--- cypress/integration/form.spec.js
+++ cypress/integration/form.spec.js
@@ -30,9 +30,21 @@ describe('Form Input', () => {
           .clear()
           .type('shake it around in the box if you have the time')
     })
-    it('can select size', () => {
+    it('can select small size', () => {
         cy.get('select[name=pizzaSize]')
           .select('Small')
+    })
+    it('can select medium size', () => {
+        cy.get('select[name=pizzaSize]')
+          .select('Medium')
+    })
+    it('can select large size', () => {
+        cy.get('select[name=pizzaSize]')
+          .select('Large')
+    })
+    it('can select extra large size', () => {
+        cy.get('select[name=pizzaSize]')
+          .select('Extra Large')
     })
 })
 
@@ -44,15 +56,40 @@ describe('Form Validation and Submit', () => {
         cy.get('div.order-container').should('not.exist')
         // test with two characters, should not create order
         cy.get('input[name=userName')
-        .type('a')
+          .type('a')
         cy.get('p.submitButton').click()
         cy.get('div.order-container').should('not.exist')
     })
     // three characters should submit an order
     it('will submit with name of 3+ characters', () => {
         cy.get('input[name=userName')
-        .type('n')
+          .type('n')
         cy.get('p.submitButton').click()
         cy.get('div.order-container').should('exist')
+    })
+})
+
+describe('Form Was Reset After Submit', () => {
+    it('has empty name text field', () => {
+        cy.get('input[name=userName')
+          .should('be.empty')
+    })
+    it('has no size selected', () => {
+        cy.get('select[name=pizzaSize]')
+          .should('have.value', '')
+    })
+    it('has no toppings selected', () => {
+        cy.get('input[name=pepperozzi]')
+          .should('not.have.attr', 'checked')
+        cy.get('input[name=muchroom]')
+          .should('not.have.attr', 'checked')
+        cy.get('input[name=octopus]')
+          .should('not.have.attr', 'checked')
+        cy.get('input[name=entitlement]')
+          .should('not.have.attr', 'checked')
+    })
+    it('has reset snarky message in instruction field', () => {
+        cy.get('textarea[name=pizzaInstructions]')
+          .should('have.value', 'Enter any additional instructions related to your sense of entitlement here!')
     })
 })
